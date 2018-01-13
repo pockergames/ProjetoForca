463421ab1145e3c49c23c5f2e061abac425c2a45
pockergames <pockergames@gmail.com>
2018-01-13 13:19:10 -0200

    Cadastro de usuários -- Sem Integração a BD
    É um começo q para o nosso código que mais tarde poderemos implementar no WPF

---
+++ TestesForca/Forca.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesForca
+{
+    class Forca
+    {
+        public string Resposta { get; set; }
+
+        public static int NumeroDeJogadores { get; set; }
+        
+
+        private static Jogador[] jogador = new Jogador[50];
+
+        public static void CadastrarUsuarios(int n)
+        {
+            NumeroDeJogadores = n;
+            for (int i = 0; i < NumeroDeJogadores; i++)
+            {
+                Console.WriteLine("Nome do Jogador {0}", i + 1);
+                jogador[i] = new Jogador();
+                jogador[i].Nome = Console.ReadLine();
+                
+            }
+        }
+
+        public static void MostrarUsuarios()
+        {
+            for (int i = 0; i < NumeroDeJogadores; i++)
+            {
+                Console.WriteLine("\nJogador{0}: {1}", i + 1, jogador[i].Nome);
+            }
+        }
+    }
+}
---
+++ TestesForca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesForca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de Usuários (Máximo de 30 Usuários)");
+            Forca.CadastrarUsuarios(int.Parse(Console.ReadLine()));
+            Forca.MostrarUsuarios();
+        }
+    }
+}
