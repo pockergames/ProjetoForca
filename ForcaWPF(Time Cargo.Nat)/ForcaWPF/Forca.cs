07d188ed761e67e0d755873bcf0188f593bb8e4c
Carlos Eduardo <pockergames@gmail.com>
2018-02-03 13:23:05 -0300

    Projeto reestruturado com a contribuição de todo o time
    Reparação de bugs;

---
+++ ForcaWPF(Time Cargo.Nat)/ForcaWPF/Forca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcaWPF
+{
+    class Forca
+    {
+        public static string Resposta { get; set; }//Resposta correta da partida. Ela recebe a palavra aleatória que vem do BD e será, posteriormente, escondida pelos traços
+        public static string Tema { get; set; }//Respectivo tema da palavra
+
+        public const int PONTUACAO_POR_LETRA = 10;
+        public const int PONTUACAO_PALAVRA_INTEIRA = 10;
+
+        public static void PegarPalavraAleatoria()//Pega uma palavra aleatória do banco de dados com seu respectivo tema
+        {
+            Resposta = "carbono";
+            Tema = "Química é louca";
+        }
+
+
+
+
+    }
+}
---
+++ ForcaWPF(Time Cargo.Nat)/ForcaWPF/Jogador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForcaWPF
+{
+    class Jogador
+    {
+        public string Nome { get; set; }//Nome do jogador
+        public int Pontuacao { get; set; }//Pontuacao do jogador
+        NomeDaClasse nomeDaIntancia = new NomeDaClasse();
+        Jogador CarlosMIranda = new Jogador();
+    }
+
+}
